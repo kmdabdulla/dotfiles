4cf7342f27b6003e2c17c86954884307d03f4033
kmdabdulla <kmohamedabdulla@gmail.com>
2023-07-23 17:38:11 -0400

    Add comments and add/remove keymaps and options

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,10 +1,19 @@
+-- Window navigation
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Move to beginning and end of the line  
+vim.keymap.set('n', '<c-a>', '_')
+vim.keymap.set('n', '<c-e>', '$')
+
+-- Stop highlighting searched word
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
+-- Paste it in different register so that you don't miss the originally copied
+-- text
+vim.keymap.set('v', '<leader>p', "\"_dP")
 
--- map Esc to jj in insert mode
+-- Map Esc to jk in insert mode
 vim.keymap.set('i', 'jk', '<Esc>')
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,20 +1,36 @@
+-- Set leader key to space
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Allow backspacing over insertion start
 vim.opt.backspace = '2'
+
+-- Show commands
 vim.opt.showcmd = true
+
+-- Always display the status bar
 vim.opt.laststatus = 2
-vim.opt.autowrite = true
+
+-- Highlight the line the cursor is currently pointing
 vim.opt.cursorline = true
+
+-- Automatically re-read files if modified outside of vim
 vim.opt.autoread = true
 
--- use spaces for tabs and whatnot
+-- Set to use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Set relative number 
+vim.wo.relativenumber = true
+
+-- Tabs and spaces
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
+-- Disable swap files
 vim.cmd [[ set noswapfile ]]
 
---Line numbers
+-- Line numbers
 vim.wo.number = true
