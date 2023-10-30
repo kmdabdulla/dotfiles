8cd98519d3034f355aa538c949ae8429390dce5b
Abdulla <kmohamedabdulla@gmail.com>
2023-10-29 22:40:44 -0400

    Enable telescope to search in hidden and gitignore directory

--- .config/nvim/lua/core/plugin_config/telescope.lua
+++ .config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,8 +1,27 @@
-require('telescope').setup()
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--no-ignore-vcs")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+  defaults = {
+    vimgrep_arguments = vimgrep_arguments,
+  },
+  pickers = {
+    find_files = {
+      find_command = { "rg", "--files", "--hidden", "--no-ignore-vcs", "--glob", "!**/.git/*" },
+    },
+  },
+})
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
--- vim.keymap.set('n', '<Space>f', builtin.find_files, {})
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Space>g', builtin.grep_string, {})
