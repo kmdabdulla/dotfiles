fce0778b716ebdeaee7caee0e5dca617784d54f6
Abdulla <kmohamedabdulla@gmail.com>
2023-10-29 22:07:08 -0400

    Show git ignore files in nvim tree

--- .config/nvim/lua/core/plugin_config/nvim-tree.lua
+++ .config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -4,7 +4,12 @@ vim.g.loaded_netrwPlugin = 1
 require("nvim-tree").setup({
   view = {
     adaptive_size = true
-  }
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
 })
 
 vim.keymap.set('n', '<c-t>', ':NvimTreeFindFileToggle<CR>')
