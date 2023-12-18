1e538959d21f87e97cbee32215ebed5c3f7e7d9f
Abdulla <kmohamedabdulla@gmail.com>
2023-12-17 21:18:36 -0500

    Enable indentation in treesitter

--- .config/nvim/lua/core/plugin_config/treesitter.lua
+++ .config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,11 +1,14 @@
 require 'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "php", "markdown", "markdown_inline" },
+    -- A list of parser names, or "all"
+    ensure_installed = { "c", "lua", "rust", "ruby", "vim", "php", "markdown", "markdown_inline" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-  },
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+    },
+    indent = {
+        enable = true,
+    },
 }
